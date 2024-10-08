3141f481779fef2810780533061d23490ed09b68
Leqso <leqso_leqso.05@mail.ru>
2024-10-08 12:44:03 +0300

    ya loh 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Cart from "./components/Cart";
+
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    imageUrl: "1.jpg",
+  },
+];
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,7 +30,16 @@ function App() {
         </div>
 
         <div className="sneakers">
-          <Card name="hi" />
+          {arr.map((val) => {
+            return (
+              <Card
+                title={val.title}
+                price={val.price}
+                imageUrl={val.imageUrl}
+                key={val.title}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,25 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <button className="favorite">
-        <img src="img/heart.svg" alt="favorite" />
+        <img src="img/card/heart.svg" alt="favorite" />
       </button>
-      <img width={133} height={112} src="img/sneakers/1.jpg" alt="sneakers" />
-      <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+      <img
+        width={133}
+        height={112}
+        src={"img/card/sneakers/" + props.imageUrl}
+        alt="sneakers"
+      />
+      <p>{props.title}</p>
       <div className="cardBottom">
         <div className="cardPrice">
           <span>ЦЕНА:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
         <button className="addToCart">
-          <img src="img/plus.svg" alt="plus" />
+          <img src="img/card/plus.svg" alt="plus" />
         </button>
       </div>
     </div>
