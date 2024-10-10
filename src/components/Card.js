764b0c42e8da57287409b2e10cd4bb6e4a91db9d
Leqso <leqso_leqso.05@mail.ru>
2024-10-10 22:50:31 +0300

    ya loh 4

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Card = (props) => {
+  const [favoriteImageUrl, setFavoriteImageUrl] = useState(0);
+  const [addToCartImageUrl, setAddToCartImageUrl] = useState("plus.svg");
+  const favoriteImageUrlArr = [{ 0: "heart.svg" }, { 1: "heartFavorite.svg" }];
   return (
     <div className="card">
-      <button className="favorite">
-        <img src="img/card/heart.svg" alt="favorite" />
+      <button
+        className="favorite"
+        onClick={() => {
+          setFavoriteImageUrl(!favoriteImageUrl);
+          console.log(favoriteImageUrl);
+        }}
+      >
+        <img
+          src={"img/card/" + favoriteImageUrlArr[favoriteImageUrl]}
+          alt="favorite"
+        />
       </button>
       <img
         width={133}
@@ -18,8 +30,13 @@ const Card = (props) => {
           <span>ЦЕНА:</span>
           <b>{props.price} руб.</b>
         </div>
-        <button className="addToCart">
-          <img src="img/card/plus.svg" alt="plus" />
+        <button
+          className="addToCart"
+          onClick={() => {
+            setAddToCartImageUrl("plusChecked.svg");
+          }}
+        >
+          <img src={"img/card/" + addToCartImageUrl} alt="plus" />
         </button>
       </div>
     </div>
